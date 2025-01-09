60343846ba1ef70c5df1935a8f9d84d3479cd4cd
Timo Schmitz <timoxschmitz@gmail.com>
2025-01-09 10:32:05 +0100

    User Consturctor added

--- src/main/java/EventManagementCore/UserManagement/User.java
+++ src/main/java/EventManagementCore/UserManagement/User.java
@@ -1,5 +1,15 @@
 package EventManagementCore.UserManagement;
 
 public class User extends UserModel{
-
+    public User(String userID, String name, String lastName, String dateOfBirth,
+                String eMailAddress, String password, int phoneNumber, boolean isAdmin) {
+        this.userID = userID;
+        this.name = name;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.eMailAddress = eMailAddress;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.isAdmin = isAdmin;
+    }
 }
--- src/test/java/EventManagementCoreTests/UserManagementTests/UserTest.java
+++ src/test/java/EventManagementCoreTests/UserManagementTests/UserTest.java
@@ -1,9 +1,17 @@
 package EventManagementCoreTests.UserManagementTests;
 
+import EventManagementCore.UserManagement.User;
+import Helper.IDGenerationHelper;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UserTest {
+    @Test
+    void createNewUserTest() {
+        User user = new User(IDGenerationHelper.generateRandomString(5), "", "", "", "", "",
+                01223, true);
 
-
+        assertTrue(user != null);
+    }
 
 }
