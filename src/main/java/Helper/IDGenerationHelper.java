f0b8481bcae71b2ffc4c249e4a0e6d1e9f6c0d0b
Timo Schmitz <timoxschmitz@gmail.com>
2025-01-09 22:17:37 +0100

    clean up after review in IDGenerationHelper.java

--- src/main/java/Helper/IDGenerationHelper.java
+++ src/main/java/Helper/IDGenerationHelper.java
@@ -2,7 +2,7 @@ package Helper;
 
 public class IDGenerationHelper {
 
-    public static final int ID_DEFAULT_LENGHT = 20;
+    public static final int ID_DEFAULT_LENGTH = 20;
 
     /**
      * method to generate random IDs for every purpose an ID has to be provided
@@ -13,7 +13,7 @@ public class IDGenerationHelper {
         java.util.Random randomizer = new java.util.Random();
 
 
-        for (int i = 0; i < ID_DEFAULT_LENGHT; i++) {
+        for (int i = 0; i < ID_DEFAULT_LENGTH; i++) {
             randomString.append(characters.charAt(randomizer.nextInt(characters.length())));
         }
 
