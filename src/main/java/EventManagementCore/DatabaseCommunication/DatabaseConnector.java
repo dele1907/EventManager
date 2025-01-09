6f0254b9d090dfc6957ea19e985fb5ede4955e52
Laura Dolibois <dolibois.laura@proton.me>
2025-01-09 17:08:55 +0100

    added database, database connection and test

--- src/main/java/EventManagementCore/DatabaseCommunication/DatabaseConnector.java
+++ src/main/java/EventManagementCore/DatabaseCommunication/DatabaseConnector.java
@@ -1,4 +1,22 @@
 package EventManagementCore.DatabaseCommunication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnector {
+
+    private static final String databaseUrl = "jdbc:sqlite:src/main/resources/eventmanager.sqlite";
+
+    public static Connection connect() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(databaseUrl);
+            System.out.println("Connection to SQLite established.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return connection;
+    }
+
 }
