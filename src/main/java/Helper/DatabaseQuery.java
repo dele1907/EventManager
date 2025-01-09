4d1c1dc6a9ee8973147b5c2c51aa22ed6afa494c
Timo Schmitz <timoxschmitz@gmail.com>
2025-01-09 23:19:38 +0100

    refactoring User.java and added DatabaseQuery.java

--- src/main/java/Helper/DatabaseQuery.java
+++ src/main/java/Helper/DatabaseQuery.java
@@ -5,6 +5,6 @@ import EventManagementCore.UserManagement.User;
 public class DatabaseQuery {
     User testUser = new User("", "", "", "", "", 0123, false);
     public static void UserIDQuery() {
-
+        //TODO @Timo Ticket #10
     }
 }
