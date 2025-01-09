02ced80fbd46044667834d20e8cab58cc4dbb8da
Laura Dolibois <dolibois.laura@proton.me>
2025-01-09 17:08:24 +0100

    added database, database connection and test

--- src/test/java/EventManagementCoreTests/DatabaseCommunicationTests/DatabaseConnectorTestDrive.java
+++ src/test/java/EventManagementCoreTests/DatabaseCommunicationTests/DatabaseConnectorTestDrive.java
@@ -1,4 +1,22 @@
 package EventManagementCoreTests.DatabaseCommunicationTests;
 
+import EventManagementCore.DatabaseCommunication.DatabaseConnector;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class DatabaseConnectorTestDrive {
+
+    // Testen auf erfolgreiche Verbindung
+    @Test
+    void testConnection() throws SQLException {
+        Connection connection = DatabaseConnector.connect();
+        assertNotNull(connection, "Connection should not be null");
+
+        connection.close();
+    }
+
 }
