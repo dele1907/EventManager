64bce39612b6f763946275ebbb490c96a2a6e456
dennisleister <dennleist@gmail.com>
2025-01-09 10:48:27 +0100

    added some documentation to methods of helper classes

--- src/main/java/Helper/IDGenerationHelper.java
+++ src/main/java/Helper/IDGenerationHelper.java
@@ -2,6 +2,10 @@ package Helper;
 
 public class IDGenerationHelper {
 
+    /**
+     * method to generate random IDs for every purpose an ID has to be provided, as longer the lenght
+     * the higher the chance that the ID stays unique
+     * */
     public static String generateRandomString(int length) {
         String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
         StringBuilder randomString = new StringBuilder();
--- src/main/java/Helper/PermissionHelper.java
+++ src/main/java/Helper/PermissionHelper.java
@@ -4,11 +4,19 @@ import EventManagementCore.PermissionRoleManagement.Permission;
 import EventManagementCore.UserManagement.User;
 
 public class PermissionHelper {
+
+    /**
+     * method which checks if the user is an admin and the permission is an admin related permission
+     * so a non admin user can not gain an admin-only permission
+     * */
     private static boolean checkAdminUserAdminPermission(User user, Permission permission) {
 
         return user.isAdmin() && permission.isAdminPermission();
     }
 
+    /**
+     * method to add a admin-checked permission to an user
+     * */
     public static boolean addPermissionToUsersPermissions(User user, Permission permission) {
         if (checkAdminUserAdminPermission(user, permission)) {
             user.addPermissionToUser(permission);
