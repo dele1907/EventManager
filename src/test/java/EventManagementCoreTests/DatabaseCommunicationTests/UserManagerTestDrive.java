96a7d0d3f2197215efbc46589878811a1b41c4c7
Laura Dolibois <dolibois.laura@proton.me>
2025-01-10 17:12:38 +0100

    updated UserManager and added Tests

--- src/test/java/EventManagementCoreTests/DatabaseCommunicationTests/UserManagerTestDrive.java
+++ src/test/java/EventManagementCoreTests/DatabaseCommunicationTests/UserManagerTestDrive.java
@@ -18,7 +18,7 @@ public class UserManagerTestDrive {
     public void setUp() {
         // Vor jedem Test eine Instanz von UserManager erstellen
         testUserManager = new UserManager();
-        testUser = new User("Max", "Mustermann", "1980-01-01",
+        testUser = new User("testUserID", "Max", "Mustermann", "1980-01-01",
                 "max.mustermann@mail.com", "password123", 1234567890, true);
     }
 
@@ -32,14 +32,49 @@ public class UserManagerTestDrive {
     }
 
     @Test
+    public void testCreateUserFailed() {
+        testUserManager.createNewUser(testUser);
+        boolean userCreated = testUserManager.createNewUser(testUser);
+        assertFalse(userCreated, "User creation was successful but should not.");
+    }
+
+    @Test
     public void testDeleteUserFailed() {
         boolean userDeleted = testUserManager.deleteUserByID("invalidID");
         assertFalse(userDeleted, "User deletion was successful but should not.");
     }
 
+    @Test
+    public void testReadUserByID() {
+        testUserManager.createNewUser(testUser);
+        User userFromDatabase = testUserManager.readUserByID("testUserID");
+        assertEquals("testUserID", userFromDatabase.getUserID());
+        assertEquals("Max", userFromDatabase.getFirstName());
+        assertEquals("Mustermann", userFromDatabase.getLastName());
+        assertEquals("1980-01-01", userFromDatabase.getDateOfBirth());
+        assertEquals("max.mustermann@mail.com", userFromDatabase.getEMailAddress());
+        assertEquals("password123", userFromDatabase.getPassword());
+        assertEquals(1234567890, userFromDatabase.getPhoneNumber());
+        assertEquals(true, userFromDatabase.isAdmin());
+    }
+
+    @Test
+    public void testReadUserByEMail() {
+        testUserManager.createNewUser(testUser);
+        User userFromDatabase = testUserManager.readUserByEMail("max.mustermann@mail.com");
+        assertEquals("testUserID", userFromDatabase.getUserID());
+        assertEquals("Max", userFromDatabase.getFirstName());
+        assertEquals("Mustermann", userFromDatabase.getLastName());
+        assertEquals("1980-01-01", userFromDatabase.getDateOfBirth());
+        assertEquals("max.mustermann@mail.com", userFromDatabase.getEMailAddress());
+        assertEquals("password123", userFromDatabase.getPassword());
+        assertEquals(1234567890, userFromDatabase.getPhoneNumber());
+        assertEquals(true, userFromDatabase.isAdmin());
+    }
+
     @AfterEach
     public void cleanUp() {
-        // ...
+        testUserManager.deleteUserByID("testUserID");
     }
 
 }
