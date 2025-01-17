e292ba968976c575964656382935fc9166fdc46c
Finn4239 <finn.oettinger@outlook.de>
2025-01-17 17:15:49 +0100

    implementing EventManagerMain

--- src/main/java/de/eventmanager/core/EventManagerMain.java
+++ src/main/java/de/eventmanager/core/EventManagerMain.java
@@ -4,19 +4,57 @@ import de.eventmanager.core.presentation.Controller.UserController;
 import de.eventmanager.core.presentation.Service.Implementation.UserServiceImpl;
 import de.eventmanager.core.presentation.Service.UserService;
 import de.eventmanager.core.presentation.UI.TextView;
+import de.eventmanager.core.presentation.UI.View;
 import de.eventmanager.core.users.User;
 
+import java.util.Optional;
+
 public class EventManagerMain {
+
+    private static View textView = new TextView();
+    private static UserService userServiceImpl = new UserServiceImpl();
+    private static UserController userController = new UserController(textView, userServiceImpl);
+    private static Optional<User> loggedInUser;
+
     public static void main(String[] args) {
-        TextView textView = new TextView();
-        UserService userService;
-        UserController userController;
-        User loggedInUser;
 
-        String choice = textView.getUserInput();
-        if (choice.equals("1")) {
-
+        try {
+            startUI();
+        }catch (Exception e) {
+            e.printStackTrace();
         }
 
+    }
+
+    private static void startUI() {
+        boolean programIsRunning = true;
+        while (programIsRunning) {
+
+            textView.displayMessage("\n===== EventManager =====\n1. Register\n2. Login\n3. Exit");
+            textView.displayMessage("\nChoose an option: ");
+            String choice = textView.getUserInput();
+
+            switch (choice) {
+                case "1":
+                    userController.registerUser();
+                    startUI();
+                    programIsRunning = false;
+                    break;
+                case "2":
+                    loggedInUser =  userController.loginUser();
+                    if (loggedInUser.isPresent()) {
+                        programIsRunning = false;
+                    }
+                    break;
+                case "3":
+                    textView.displayMessage("\nExit Program...");
+                    programIsRunning = false;
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("\nInvalid choice");
+                    break;
+            }
+        }
     }
 }
