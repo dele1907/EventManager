005900ae146aaf5949ffd41770f01fe9e37fc227
Laura Dolibois <dolibois.laura@proton.me>
2025-01-17 14:55:18 +0100

    added attributes and methods to EventModel

--- src/main/java/de/eventmanager/core/events/EventModel.java
+++ src/main/java/de/eventmanager/core/events/EventModel.java
@@ -1,4 +1,95 @@
 package de.eventmanager.core.events;
 
+import de.eventmanager.core.users.User;
+
+import java.util.ArrayList;
+
 public abstract class EventModel {
+
+    private String eventID;
+    private String eventName;
+    private String eventDateTime;
+    private int numberOfBookedUsersOnEvent;
+    private ArrayList<User> bookedUsersOnEvent;
+    private String category;
+    private boolean privateEvent;
+
+    // #region CRUD-Operationen
+    public abstract boolean createNewEvent(
+            String eventName,
+            String eventDateTime,
+            String category,
+            boolean privateEvent
+    );
+
+    public abstract EventModel getEventByID(String eventID);
+
+    public abstract void editEvent(
+            String eventID,
+            String eventName,
+            String eventDateTime,
+            ArrayList<User> bookedUsersOnEvent,
+            String category,
+            boolean privateEvent
+    );
+
+    public abstract boolean deleteEvent(String eventID);
+    // #endregion CRUD-Operationen
+
+    // #region Getter
+    public String getEventID() {
+        return eventID;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public String getEventDateTime() {
+        return eventDateTime;
+    }
+
+    public int getNumberOfBookedUsersOnEvent() {
+        return numberOfBookedUsersOnEvent;
+    }
+
+    public ArrayList<User> getBookedUsersOnEvent() {
+        return bookedUsersOnEvent;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public boolean isPrivateEvent() {
+        return privateEvent;
+    }
+    // #endregion Getter
+
+    // #region Setter
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public void setEventDateTime(String eventDateTime) {
+        this.eventDateTime = eventDateTime;
+    }
+
+    public void setNumberOfBookedUsersOnEvent(int numberOfBookedUsersOnEvent) {
+        this.numberOfBookedUsersOnEvent = numberOfBookedUsersOnEvent;
+    }
+
+    public void setBookedUsersOnEvent(ArrayList<User> bookedUsersOnEvent) {
+        this.bookedUsersOnEvent = bookedUsersOnEvent;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setPrivateEvent(boolean privateEvent) {
+        this.privateEvent = privateEvent;
+    }
+    // #endregion Setter
+
 }
