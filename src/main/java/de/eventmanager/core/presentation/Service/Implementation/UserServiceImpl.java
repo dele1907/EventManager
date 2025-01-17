a31fb0d22c883ce0c94b753b970668a85ed57a11
dennisleister <dennleist@gmail.com>
2025-01-17 15:40:27 +0100

    first methods for UserServiceImpl

--- src/main/java/de/eventmanager/core/presentation/Service/Implementation/UserServiceImpl.java
+++ src/main/java/de/eventmanager/core/presentation/Service/Implementation/UserServiceImpl.java
@@ -1,4 +1,36 @@
 package de.eventmanager.core.presentation.Service.Implementation;
 
-public class UserServiceImpl {
+import de.eventmanager.core.presentation.Service.UserService;
+import de.eventmanager.core.users.Management.UserManager;
+import de.eventmanager.core.users.User;
+
+import java.util.Optional;
+
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public boolean registerUser(
+            String firstName,
+            String lastName,
+            String dateOfBirth,
+            String email,
+            String password,
+            String passwordConfirmation,
+            int phoneNumber) {
+
+        if (!UserManager.isValidRegistrationPassword(password, passwordConfirmation)) {
+            return false;
+        }
+
+        return UserManager.createNewUser(new User(firstName, lastName, dateOfBirth, email, password, phoneNumber));
+    }
+
+    @Override
+    public Optional<User> loginUser(String email, String password) {
+        if (!UserManager.authenticationUserLogin(email, password)) {
+            return Optional.empty();
+        }
+
+        return Optional.of(UserManager.readUserByEMail(email));
+    }
 }
--- src/main/java/de/eventmanager/core/presentation/Service/UserService.java
+++ src/main/java/de/eventmanager/core/presentation/Service/UserService.java
@@ -2,7 +2,9 @@ package de.eventmanager.core.presentation.Service;
 
 import de.eventmanager.core.users.User;
 
+import java.util.Optional;
+
 public interface UserService {
-    boolean registerUser(String firstName, String lastName, String dateOfBirth, String email, String password, int phoneNumber);
-    User loginUser(String email, String password);
+    boolean registerUser(String firstName, String lastName, String dateOfBirth, String email, String password, String passwordConfirmation, int phoneNumber);
+    Optional<User> loginUser(String email, String password);
 }
