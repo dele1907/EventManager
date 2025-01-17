29ba3bba7d7ca04d91bf7fe564221abe309219de
Finn4239 <finn.oettinger@outlook.de>
2025-01-17 15:13:26 +0100

    implement UI-Interface methods

--- src/main/java/de/eventmanager/core/presentation/Service/UserService.java
+++ src/main/java/de/eventmanager/core/presentation/Service/UserService.java
@@ -1,4 +1,6 @@
 package de.eventmanager.core.presentation.Service;
 
 public interface UserService {
+    void displayMessage(String message);
+    String getUserInput();
 }
--- src/main/java/de/eventmanager/core/presentation/UI/View.java
+++ src/main/java/de/eventmanager/core/presentation/UI/View.java
@@ -1,4 +1,8 @@
 package de.eventmanager.core.presentation.UI;
 
+import de.eventmanager.core.users.User;
+
 public interface View {
+    boolean registerUser(String firstName, String lastName, String dateOfBirth, String email, String password, int phoneNumber);
+    User loginUser(String email, String password);
 }
