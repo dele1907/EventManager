88f434b7afbeefa17ded37ba078e130b232da048
Laura Dolibois <dolibois.laura@proton.me>
2025-01-18 14:35:29 +0100

    added EventManagerTestDrive and implemented tests

---
+++ src/test/java/de/eventmanager/core/events/Management/EventManagerTestDrive.java
@@ -0,0 +1,144 @@
+package de.eventmanager.core.events.Management;
+
+import de.eventmanager.core.events.PrivateEvent;
+import de.eventmanager.core.events.PublicEvent;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class EventManagerTestDrive {
+
+    private PrivateEvent testPrivateEvent;
+    private PrivateEvent testPrivateEventUpdated;
+    private PublicEvent testPublicEvent;
+    private PublicEvent testPublicEventUpdated;
+    private boolean skipSetUp = false;
+    private boolean skipCleanUp = false;
+
+    // Vor jedem Test je zwei private und öffentliche Test-Events erstellen:
+    @BeforeEach
+    public void setUp() {
+
+        if(skipSetUp) {
+
+            return;
+        }
+
+        testPrivateEvent = new PrivateEvent("testPrivateEventID", "Geburtstag von Oma", "2025-11-11", 0, "private Feier", true);
+        testPrivateEventUpdated = new PrivateEvent("testPrivateEventID", "Weihnachtsfeier", "2025-12-12", 0, "Firmenfeier", true);
+
+        testPublicEvent = new PublicEvent("testPublicEventID", "Ostermarkt", "2025-04-04", 0, "Markt", false, 2000);
+        testPublicEventUpdated = new PublicEvent("testPublicEventID", "Kirmes", "2025-06-06", 0, "Dorffest", false, 5000);
+    }
+
+    // Nach jedem Test die Datenbank bereinigen:
+    @AfterEach
+    public void cleanUp() {
+
+        if(skipCleanUp) {
+
+            return;
+        }
+
+        EventManager.deleteEventByID("testPrivateEventID");
+        EventManager.deleteEventByID("testPublicEventID");
+    }
+
+    // Testen von Erstellen, Updaten und Löschen eines Events in der Datenbank:
+    @Test
+    @Order(0)
+    public void testCreateUpdateDeleteEvent() {
+
+        skipCleanUp = true;
+
+        boolean privateEventCreated = EventManager.createNewEvent(testPrivateEvent);
+        boolean publicEventCreated = EventManager.createNewEvent(testPublicEvent);
+
+        assertTrue(privateEventCreated, "Event creation failed but should not.");
+        assertTrue(publicEventCreated, "Event creation failed but should not.");
+
+        boolean privateEventUpdated = EventManager.updateEvent(testPrivateEventUpdated);
+        boolean publicEventUpdated = EventManager.updateEvent(testPublicEventUpdated);
+
+        assertTrue(privateEventUpdated, "Event update failed but should not.");
+        assertTrue(publicEventUpdated, "Event update failed but should not.");
+
+        boolean privateEventDeleted = EventManager.deleteEventByID(testPrivateEvent.getEventID());
+        boolean publicEventDeleted = EventManager.deleteEventByID(testPublicEvent.getEventID());
+
+        assertTrue(privateEventDeleted, "Event deletion failed but should not.");
+        assertTrue(publicEventDeleted, "Event deletion failed but should not.");
+    }
+
+    // Testen, ob ein Event nicht mehrmals erstellt werden kann:
+    @Test
+    @Order(1)
+    public void testCreateEventFailed() {
+
+        EventManager.createNewEvent(testPrivateEvent);
+        boolean privateEventCreated = EventManager.createNewEvent(testPrivateEvent);
+
+        EventManager.createNewEvent(testPublicEvent);
+        boolean publicEventCreated = EventManager.createNewEvent(testPublicEvent);
+
+        assertFalse(privateEventCreated, "Event creation was successful but should not.");
+        assertFalse(publicEventCreated, "Event creation was successful but should not.");
+    }
+
+    // Testen, ob ein Update nur möglich ist, wenn der entsprechende Datensatz vorliegt:
+    @Test
+    @Order(2)
+    public void testUpdateEventFailed() {
+
+        skipCleanUp = true;
+
+        boolean privateEventUpdated = EventManager.updateEvent(testPrivateEventUpdated);
+        boolean publicEventUpdated = EventManager.updateEvent(testPublicEventUpdated);
+
+        assertFalse(privateEventUpdated, "Event update was successful but should not.");
+        assertFalse(publicEventUpdated, "Event update was successful but should not.");
+    }
+
+    // Testen, ob Löschen nur möglich ist, wenn der entsprechende Datensatz vorliegt:
+    @Test
+    @Order(3)
+    public void testDeleteEventFailed() {
+
+        skipSetUp = true;
+        skipCleanUp = true;
+
+        boolean eventDeleted = EventManager.deleteEventByID("invalidID");
+
+        assertFalse(eventDeleted, "Event deletion was successful but should not.");
+    }
+
+    // Testen, ob ein Event anhand der ID korrekt ausgelesen werden kann:
+    @Test
+    @Order(4)
+    public void testReadEventByID() {
+
+        EventManager.createNewEvent(testPrivateEvent);
+        EventManager.createNewEvent(testPublicEvent);
+
+        PrivateEvent privateEventFromDatabase = (PrivateEvent) EventManager.readPrivateEventByID("testPrivateEventID");
+        PublicEvent publicEventFromDatabase = (PublicEvent) EventManager.readPublicEventByID("testPublicEventID");
+
+        assertEquals("testPrivateEventID", privateEventFromDatabase.getEventID());
+        assertEquals("Geburtstag von Oma", privateEventFromDatabase.getEventName());
+        assertEquals("2025-11-11", privateEventFromDatabase.getEventDateTime());
+        assertEquals(0, privateEventFromDatabase.getNumberOfBookedUsersOnEvent());
+        assertEquals("private Feier", privateEventFromDatabase.getCategory());
+        assertEquals(true, privateEventFromDatabase.isPrivateEvent());
+
+        assertEquals("testPublicEventID", publicEventFromDatabase.getEventID());
+        assertEquals("Ostermarkt", publicEventFromDatabase.getEventName());
+        assertEquals("2025-04-04", publicEventFromDatabase.getEventDateTime());
+        assertEquals(0, publicEventFromDatabase.getNumberOfBookedUsersOnEvent());
+        assertEquals("Markt", publicEventFromDatabase.getCategory());
+        assertEquals(false, publicEventFromDatabase.isPrivateEvent());
+        assertEquals(2000, publicEventFromDatabase.getMaximumCapacity());
+    }
+}
