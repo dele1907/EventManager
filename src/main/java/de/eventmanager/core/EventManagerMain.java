c2c8e6caf90969a0951a22099d76c6d923c16800
dennisleister <dennleist@gmail.com>
2025-01-19 14:19:33 +0100

    Added EventManagerTextBasedUIInstance so in Main to decouple Main from classes of the text based UI

--- src/main/java/de/eventmanager/core/EventManagerMain.java
+++ src/main/java/de/eventmanager/core/EventManagerMain.java
@@ -1,39 +1,11 @@
 package de.eventmanager.core;
 
-import de.eventmanager.core.presentation.Controller.UserController;
-import de.eventmanager.core.presentation.Service.Implementation.UserServiceImpl;
-import de.eventmanager.core.presentation.Service.UserService;
-import de.eventmanager.core.presentation.UI.Tabs.LoginTab;
-import de.eventmanager.core.presentation.UI.Tabs.MainMenuTab;
-import de.eventmanager.core.presentation.UI.TextView;
-import de.eventmanager.core.presentation.UI.View;
-import de.eventmanager.core.users.User;
-
-import java.util.Optional;
+import de.eventmanager.core.presentation.EventManagerTextBasedUIInstance;
 
 public class EventManagerMain {
-
-    private static View textView = new TextView();
-    private static UserService userServiceImpl = new UserServiceImpl();
-    private static UserController userController = new UserController(textView, userServiceImpl);
-    private static Optional<User> loggedInUser;
-    private static LoginTab loginTab = new LoginTab(textView, userController);
+    private static EventManagerTextBasedUIInstance eventManagerTextBasedUIInstance = new EventManagerTextBasedUIInstance();
 
     public static void main(String[] args) {
-        boolean programIsRunning = true;
-
-        while (programIsRunning) {
-            try {
-                loginTab.start();
-                loggedInUser = loginTab.getLoggedInUser();
-
-                if (loggedInUser.isPresent()) {
-                    MainMenuTab mainMenuTab = new MainMenuTab(textView, loggedInUser.get(), loginTab, userController);
-                    mainMenuTab.start();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+        eventManagerTextBasedUIInstance.startEventManagerInstance();
     }
 }
---
+++ src/main/java/de/eventmanager/core/presentation/EventManagerTextBasedUIInstance.java
@@ -0,0 +1,38 @@
+package de.eventmanager.core.presentation;
+
+import de.eventmanager.core.presentation.Controller.UserController;
+import de.eventmanager.core.presentation.Service.Implementation.UserServiceImpl;
+import de.eventmanager.core.presentation.Service.UserService;
+import de.eventmanager.core.presentation.UI.Tabs.LoginTab;
+import de.eventmanager.core.presentation.UI.Tabs.MainMenuTab;
+import de.eventmanager.core.presentation.UI.TextView;
+import de.eventmanager.core.presentation.UI.View;
+import de.eventmanager.core.users.User;
+
+import java.util.Optional;
+
+public class EventManagerTextBasedUIInstance {
+    private static View textView = new TextView();
+    private static UserService userServiceImpl = new UserServiceImpl();
+    private static UserController userController = new UserController(textView, userServiceImpl);
+    private static Optional<User> loggedInUser;
+    private static LoginTab loginTab = new LoginTab(textView, userController);
+
+public void startEventManagerInstance() {
+    boolean programIsRunning = true;
+
+    while (programIsRunning) {
+        try {
+            loginTab.start();
+            loggedInUser = loginTab.getLoggedInUser();
+
+            if (loggedInUser.isPresent()) {
+                MainMenuTab mainMenuTab = new MainMenuTab(textView, loggedInUser.get(), loginTab, userController);
+                mainMenuTab.start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+}
