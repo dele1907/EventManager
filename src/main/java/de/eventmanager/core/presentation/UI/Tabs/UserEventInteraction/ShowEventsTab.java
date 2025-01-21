d54560bdce458de4d066fb59cd50823dc1daea9a
Timo Schmitz <timoxschmitz@gmail.com>
2025-01-21 20:42:21 +0100

    Working on ShowEventsTab.java addet some methods

--- src/main/java/de/eventmanager/core/presentation/UI/Tabs/UserEventInteraction/ShowEventsTab.java
+++ src/main/java/de/eventmanager/core/presentation/UI/Tabs/UserEventInteraction/ShowEventsTab.java
@@ -1,6 +1,8 @@
 package de.eventmanager.core.presentation.UI.Tabs.UserEventInteraction;
 
 import de.eventmanager.core.presentation.Controller.UserController;
+import de.eventmanager.core.presentation.UI.Tabs.LoginTab;
+import de.eventmanager.core.presentation.UI.Tabs.MainMenuTab;
 import de.eventmanager.core.presentation.UI.Tabs.Tab;
 import de.eventmanager.core.presentation.UI.View;
 import de.eventmanager.core.users.User;
@@ -19,7 +21,7 @@ public class ShowEventsTab implements Tab{
     public void start() {
 
         System.out.println("===== ShowEventsTab =====");
-        textView.displayMessage("\n1. Search by Name \n2. Search by Location \n3. Search by City");
+        textView.displayMessage("\n1. Search by Name \n2. Search by Location \n3. Search by City \n4. Go Back to Main Menu");
 
         textView.displayMessage("\nChoose an option: ");
         String choice = textView.getUserInput();
@@ -37,10 +39,23 @@ public class ShowEventsTab implements Tab{
                 getEventInformationbyName(eventName);
                 break;
             case "2":
-                textView.displayErrorMessage("\npage not implemented yet");
+                textView.displayMessage("\nWhats the Location of the Event you looking for?");
+                textView.displayMessage("\nType in: ");
+                String eventLocation = textView.getUserInput();
+                getEventInformationbyLocation(eventLocation);
                 break;
             case "3":
-                textView.displayErrorMessage("\npage not implemented yet");
+                textView.displayMessage("\nWhats the City of the Event you looking for?");
+                textView.displayMessage("\nType in: ");
+                String eventCity = textView.getUserInput();
+                getEventInformationbyCity(eventCity);
+                break;
+            case "4":
+                textView.displayMessage("\nGo Back to Main Menu");
+                handelGoBackToMainMenu();
+                break;
+            default:
+                textView.displayErrorMessage("\nPlease enter a valid option!");
                 break;
         }
 
@@ -49,5 +64,29 @@ public class ShowEventsTab implements Tab{
 
     public void getEventInformationbyName(String eventName) {
 
+    }
+
+    public void getEventInformationbyLocation(String eventLocation) {
+
+    }
+
+    public void getEventInformationbyCity(String eventCity) {
+
+    }
+
+    private void addDelay(int seconds) {
+        int delay = seconds * 1000;
+
+        try{
+            Thread.sleep(delay);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private void handelGoBackToMainMenu() {
+        MainMenuTab mainMenuTab = new MainMenuTab(textView, loggedInUser, new LoginTab(textView, userController), userController);
+        mainMenuTab.start();
+        addDelay(2);
     }
 }
