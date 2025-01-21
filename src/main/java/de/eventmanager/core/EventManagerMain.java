56a7eeb9bd34c3991ef0a000af4b4cd00ab6a8f3
dennisleister <dennleist@gmail.com>
2025-01-21 20:21:09 +0100

    naming adjusted

--- src/main/java/de/eventmanager/core/EventManagerMain.java
+++ src/main/java/de/eventmanager/core/EventManagerMain.java
@@ -1,9 +1,10 @@
 package de.eventmanager.core;
 
+import de.eventmanager.core.presentation.EventManagerInstance;
 import de.eventmanager.core.presentation.EventManagerTextBasedUIInstance;
 
 public class EventManagerMain {
-    private static EventManagerTextBasedUIInstance eventManagerTextBasedUIInstance = new EventManagerTextBasedUIInstance();
+    private static EventManagerInstance eventManagerTextBasedUIInstance = new EventManagerTextBasedUIInstance();
 
     public static void main(String[] args) {
         eventManagerTextBasedUIInstance.startEventManagerInstance();
