9140715732a3ece2a3038a002a97cdb6fabb8897
Finn4239 <finn.oettinger@outlook.de>
2025-01-26 16:12:45 +0100

    change HelperClass for GeneralUse

--- src/main/java/helper/ListFormattingHelper.java
+++ src/main/java/helper/ListFormattingHelper.java
@@ -1,7 +1,29 @@
 package helper;
 
+
 public class ListFormattingHelper {
-    public final static int COLUMN_WIDTH = 20;
-    public final static String headerFormat = "%-" + COLUMN_WIDTH + "s%-" + COLUMN_WIDTH + "s%-" + COLUMN_WIDTH + "s%-" + COLUMN_WIDTH + "s%n";
-    public final static String rowFormat = "%-" + COLUMN_WIDTH + "s%-" + COLUMN_WIDTH + "s%-" + COLUMN_WIDTH + "s%-" + COLUMN_WIDTH + "s%n";
+    public final static int ROW_WIDTH = 20;
+    public final static int ROW_WIDTH_AFTER_EMAIL = 30;;
+
+    public static String rowFormat(int row, int emailPosition) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i<row; i++) {
+
+
+            if (i == emailPosition-1) {
+
+                sb.append("%-").append(ROW_WIDTH_AFTER_EMAIL).append("s");
+            } else if (i == row - 1) {
+
+                sb.append("%-").append(ROW_WIDTH).append("s%n");
+            } else {
+
+                sb.append("%-").append(ROW_WIDTH).append("s");
+            }
+        }
+
+        return sb.toString();
+    }
+
 }
