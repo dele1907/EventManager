637bf006cb96b47a72f263f80c5c953fcda015bd
Finn4239 <finn.oettinger@outlook.de>
2025-01-26 15:43:23 +0100

    add new HelperClass for ParticipantList

--- src/main/java/helper/ListFormattingHelper.java
+++ src/main/java/helper/ListFormattingHelper.java
@@ -4,5 +4,4 @@ public class ListFormattingHelper {
     public final static int COLUMN_WIDTH = 20;
     public final static String headerFormat = "%-" + COLUMN_WIDTH + "s%-" + COLUMN_WIDTH + "s%-" + COLUMN_WIDTH + "s%-" + COLUMN_WIDTH + "s%n";
     public final static String rowFormat = "%-" + COLUMN_WIDTH + "s%-" + COLUMN_WIDTH + "s%-" + COLUMN_WIDTH + "s%-" + COLUMN_WIDTH + "s%n";
-
 }
