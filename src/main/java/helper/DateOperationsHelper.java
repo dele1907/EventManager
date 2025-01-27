587a9f41544c44d7fb18033bf96c953a73dd85c7
Timo Schmitz <timoxschmitz@gmail.com>
2025-01-27 17:55:27 +0100

    small chnage

--- src/main/java/helper/DateOperationsHelper.java
+++ src/main/java/helper/DateOperationsHelper.java
@@ -3,25 +3,28 @@ package helper;
 import de.eventmanager.core.database.Communication.DatabaseConnector;
 import de.eventmanager.core.database.Communication.EventDatabaseConnector;
 import org.jooq.DSLContext;
+import org.jooq.Select;
 import org.jooq.impl.DSL;
 import java.sql.Connection;
+import org.jooq.Record;
 import static org.jooq.generated.Tables.EVENTS;
-
 
 public class DateOperationsHelper {
 
     private static final String CONNECTION_FAIL = "The Databaseconnection fails!";
 
-    public void validateTheAge(String eventName) {
+    public void validateTheAgeProbe(String eventName) {
 
         try (Connection connection = DatabaseConnector.connect()){
 
             DSLContext create = DSL.using(connection);
 
-            create.select()
+                  Record record = create.select(EVENTS.EVENTSTART)
                     .from(EVENTS)
                     .where(EVENTS.EVENTNAME.eq(eventName))
                     .fetchOne();
+
+                  record.getValue(EVENTS.EVENTSTART);
 
         } catch (Exception exception) {
             LoggerHelper.logErrorMessage(EventDatabaseConnector.class, CONNECTION_FAIL + exception.getMessage());
