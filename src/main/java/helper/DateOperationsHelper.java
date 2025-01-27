68e49a974fc00bef89697dceedcfa9f0c912addb
Timo Schmitz <timoxschmitz@gmail.com>
2025-01-27 21:30:59 +0100

    Added two methods to DateOperationsHelper.java

--- src/main/java/helper/DateOperationsHelper.java
+++ src/main/java/helper/DateOperationsHelper.java
@@ -8,23 +8,59 @@ import org.jooq.impl.DSL;
 import java.sql.Connection;
 import org.jooq.Record;
 import static org.jooq.generated.Tables.EVENTS;
+import static org.jooq.impl.DSL.*;
+import org.jooq.*;
 
 public class DateOperationsHelper {
 
     private static final String CONNECTION_FAIL = "The Databaseconnection fails!";
 
-    public void validateTheAgeProbe(String eventName) {
+    public void validateTheAge(String name) {
 
         try (Connection connection = DatabaseConnector.connect()){
 
             DSLContext create = DSL.using(connection);
 
-                  Record record = create.select(EVENTS.EVENTSTART)
-                    .from(EVENTS)
-                    .where(EVENTS.EVENTNAME.eq(eventName))
-                    .fetchOne();
+            Field<Integer> daytimeField = field(
+                    "TIMEDIFF(datetime('now'), (SELECT birthDate FROM user WHERE firstName = {0}))",
+                    Integer.class,
+                    inline(name)
+            ).as("yeardiff");
 
-                  record.getValue(EVENTS.EVENTSTART);
+            Result<Record1<Integer>> result = create.select(daytimeField).fetch();
+
+            for (Record1<Integer> record : result) {
+                int rechnung = record.get(daytimeField);
+                System.out.println("Jahr: " + record.get("yeardiff"));
+                if (rechnung >= 18) {
+                    System.out.println("Test: " + rechnung);
+                } else {
+                    System.out.println("Er ist keine 18!");
+                }
+            }
+
+        } catch (Exception exception) {
+            LoggerHelper.logErrorMessage(EventDatabaseConnector.class, CONNECTION_FAIL + exception.getMessage());
+        }
+    }
+
+    public void timeToEvent(String eventName) {
+
+        try (Connection connection = DatabaseConnector.connect()){
+
+            DSLContext create = DSL.using(connection);
+
+            Field<String> daytimeField = field(
+                    "TIMEDIFF(datetime('now'), (SELECT eventStart FROM events WHERE eventName = {0}))",
+                    String.class,
+                    inline(eventName)
+            ).as("daydiff");
+
+            Result<Record1<String>> result = create.select(daytimeField).fetch();
+
+            for (Record1<String> record : result) {
+                System.out.println("days: " + record.get("daydiff"));
+            }
 
         } catch (Exception exception) {
             LoggerHelper.logErrorMessage(EventDatabaseConnector.class, CONNECTION_FAIL + exception.getMessage());
