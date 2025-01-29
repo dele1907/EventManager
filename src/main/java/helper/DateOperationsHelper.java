3b03fc12163feba60aae6c0249f4db676d23a895
Timo Schmitz <timoxschmitz@gmail.com>
2025-01-29 17:40:02 +0100

    small refactorting at DateOperationsHelper.java

--- src/main/java/helper/DateOperationsHelper.java
+++ src/main/java/helper/DateOperationsHelper.java
@@ -7,6 +7,7 @@ import org.jooq.impl.DSL;
 import java.sql.Connection;
 
 import org.jooq.*;
+import org.jooq.impl.QOM;
 import org.jooq.impl.SQLDataType;
 import org.jooq.types.DayToSecond;
 
@@ -78,74 +79,53 @@ public class DateOperationsHelper {
         return days;
     }
 
-    /**
-     * TODO @TIMO, @Dennis: comment in if we want to use jooq queries
-     * package helper;
-     *
-     * import de.eventmanager.core.database.Communication.DatabaseConnector;
-     * import de.eventmanager.core.database.Communication.EventDatabaseConnector;
-     * import org.jooq.DSLContext;
-     * import org.jooq.Record1;
-     * import org.jooq.Result;
-     * import org.jooq.impl.DSL;
-     * import org.jooq.impl.SQLDataType;
-     * import org.jooq.types.DayToSecond;
-     *
-     * import java.sql.Connection;
-     *
-     * public class DateOperationsHelper {
-     *
-     *     private static final String CONNECTION_FAIL = "The Database connection fails!";
-     *
-     *     public void validateTheAge(String name) {
-     *         try (Connection connection = DatabaseConnector.connect()) {
-     *             DSLContext create = DSL.using(connection);
-     *             Result<Record1<DayToSecond>> result = create.select(
-     *                     DSL.timestampDiff(DSL.currentTimestamp(),
-     *                     DSL.field("birthDate", SQLDataType.TIMESTAMP)))
-     *                     .from("user")
-     *                     .where(DSL.field("firstName").eq(name))
-     *                     .fetch();
-     *
-     *             for (Record1<DayToSecond> record : result) {
-     *                 int rechnung = record.value1().getDays() / 365;
-     *                 System.out.println("Jahr: " + rechnung);
-     *                 if (rechnung >= 18) {
-     *                     System.out.println("Ueber 18: " + (rechnung > 18));
-     *                 } else {
-     *                     System.out.println("Er ist keine 18!");
-     *                 }
-     *             }
-     *         } catch (Exception exception) {
-     *             LoggerHelper.logErrorMessage(EventDatabaseConnector.class, CONNECTION_FAIL + exception.getMessage());
-     *         }
-     *     }
-     *
-     *     public void timeToEvent(String eventName) {
-     *         try (Connection connection = DatabaseConnector.connect()) {
-     *             DSLContext create = DSL.using(connection);
-     *             Result<Record1<DayToSecond>> result = create.select(DSL.timestampDiff(DSL.currentTimestamp(), DSL.field("eventStart", SQLDataType.TIMESTAMP)))
-     *                     .from("events")
-     *                     .where(DSL.field("eventName").eq(eventName))
-     *                     .fetch();
-     *
-     *             for (Record1<DayToSecond> record : result) {
-     *                 int months = record.value1().getDays() / 30;
-     *                 System.out.println("months: " + months);
-     *                 System.out.println("days: " + (record.value1().getDays() - (months * 30)));
-     *                 System.out.println("hours: " + record.value1().getHours());
-     *                 System.out.println("minutes: " + record.value1().getMinutes());
-     *             }
-     *         } catch (Exception exception) {
-     *             LoggerHelper.logErrorMessage(EventDatabaseConnector.class, CONNECTION_FAIL + exception.getMessage());
-     *         }
-     *     }
-     *
-     *     public static void main(String[] args) {
-     *         DateOperationsHelper dateOperationsHelper = new DateOperationsHelper();
-     *         dateOperationsHelper.validateTheAge("Timo");
-     *         dateOperationsHelper.timeToEvent("Genetikk Tour");
-     *     }
-     * }
-     * */
+    public void getDayTime(String eventName) {
+
+        try (Connection connection = DatabaseConnector.connect()) {
+
+            DSLContext create = DSL.using(connection);
+
+            Result<Record1<String>> result = create.select(DSL.field("(strftime('%P', eventStart))", String.class))
+                    .from("events")
+                    .where(DSL.field("eventName").eq(eventName))
+                    .fetch();
+
+            if(result.isNotEmpty()) {
+
+                for (Record1<String> record : result) {
+                    System.out.println(record.value1());
+                }
+
+            } else {
+                LoggerHelper.logErrorMessage(EventDatabaseConnector.class, NO_EVENTSART_FOUND);
+            }
+        } catch (Exception exception) {
+            LoggerHelper.logErrorMessage(EventDatabaseConnector.class, CONNECTION_FAIL + exception.getMessage());
+        }
+    }
+
+    public void whichWeekIsTheEvent(String eventName) {
+
+        try (Connection connection = DatabaseConnector.connect()) {
+
+            DSLContext create = DSL.using(connection);
+
+            Result<Record1<String>> result = create.select(DSL.field("(strftime('%V', eventStart))", String.class))
+                    .from("events")
+                    .where(DSL.field("eventName").eq(eventName))
+                    .fetch();
+
+            if(result.isNotEmpty()) {
+
+                for (Record1<String> record : result) {
+                    System.out.println(record.value1());
+                }
+
+            } else {
+                LoggerHelper.logErrorMessage(EventDatabaseConnector.class, NO_EVENTSART_FOUND);
+            }
+        } catch (Exception exception) {
+            LoggerHelper.logErrorMessage(EventDatabaseConnector.class, CONNECTION_FAIL + exception.getMessage());
+        }
+    }
 }
