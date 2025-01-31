0d6cb0a68b75967f0461382dd22c1ef35a2b07c3
Laura Dolibois <dolibois.laura@proton.me>
2025-01-31 22:31:00 +0100

    added test classes for booking and create

---
+++ src/test/java/de/eventmanager/core/database/Communication/CreatorDatabaseConnectorTestDrive.java
@@ -0,0 +1,29 @@
+package de.eventmanager.core.database.Communication;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CreatorDatabaseConnectorTestDrive {
+
+    // TODO: rework tests
+
+    /**
+     * Test relation between event and creator (user)
+     * */
+    /*@Test
+    public void testAddAndRemoveUserCreatedEvent() {
+
+        skipSetUp = true;
+        skipCleanUp = true;
+
+        boolean testAddCreation = CreatorDatabaseConnector.assignUserAsEventCreator("testEventID", "testCreatorID");
+
+        assertTrue(testAddCreation, "Adding user to created event failed but should not.");
+
+        boolean testDeleteCreation = CreatorDatabaseConnector.removeUserAsEventCreator("testEventID", "testCreatorID");
+
+        assertTrue(testDeleteCreation, "Adding user to created event failed but should not.");
+    }*/
+
+}
