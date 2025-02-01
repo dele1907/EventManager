7bdfdd0aff5a8a05d862356813c993a64980d598
dennisleister <dennleist@gmail.com>
2025-02-01 23:21:20 +0100

    Add age validation methods to ValidationHelper and update UserServiceImpl

--- src/main/java/de/eventmanager/core/presentation/PresentationHelpers/ValidationHelper.java
+++ src/main/java/de/eventmanager/core/presentation/PresentationHelpers/ValidationHelper.java
@@ -1,5 +1,9 @@
 package de.eventmanager.core.presentation.PresentationHelpers;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
 public class ValidationHelper {
 
     public static boolean validatePhoneNumberInput(String phoneNumber) {
@@ -12,5 +16,16 @@ public class ValidationHelper {
 
     public static boolean validateDateInput(String date) {
         return date.matches("^(19[0-9]{2}|20[0-2][0-9]|2030)-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$");
+    }
+
+    private static int getAgeFromBirthdate(String birthdate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate birthDate = LocalDate.parse(birthdate, formatter);
+        LocalDate currentDate = LocalDate.now();
+        return Period.between(birthDate, currentDate).getYears();
+    }
+
+    public static boolean validateAge(String birthdate) {
+        return getAgeFromBirthdate(birthdate) >= 12 && getAgeFromBirthdate(birthdate) <= 130;
     }
 }
--- src/main/java/de/eventmanager/core/presentation/Service/UserService.java
+++ src/main/java/de/eventmanager/core/presentation/Service/UserService.java
@@ -19,4 +19,5 @@ public interface UserService {
     boolean getUserIsAdminUser(String userID);
     String getLoggedInUserName(String userID);
     String getUserInformationByEmail(String email);
+
 }
