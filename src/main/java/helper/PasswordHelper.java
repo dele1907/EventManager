b41902c2e4ef64bf3a880bbdacf108ca73115e31
dennisleister <dennleist@gmail.com>
2025-02-05 20:29:15 +0100

    refactor: switch password encoding to Argon2 for enhanced security

--- src/main/java/helper/PasswordHelper.java
+++ src/main/java/helper/PasswordHelper.java
@@ -1,17 +1,11 @@
 package helper;
 
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class PasswordHelper {
 
-    /**
-     * TODO @Dennis: Comment in when change to Argon2, also add to pom.xml:
-     *
-     * */
     private static final PasswordEncoder passwordEncoder = new Argon2PasswordEncoder();
-    //private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     public static String hashPassword(String plainPassword) {
         return passwordEncoder.encode(plainPassword);
@@ -19,9 +13,5 @@ public class PasswordHelper {
 
     public static boolean verifyPassword(String plainPassword, String hashedPassword) {
         return passwordEncoder.matches(plainPassword, hashedPassword);
-    }
-
-    public static void main(String[] args) {
-        System.out.println(hashPassword("password"));
     }
 }
