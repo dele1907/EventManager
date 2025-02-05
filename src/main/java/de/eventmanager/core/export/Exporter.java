5420912e7b5a53565620d1560ba8885e9aaf9529
Finn4239 <finn.oettinger@outlook.de>
2025-02-05 21:41:20 +0100

    Exporter-Class safe ics-Calendar-file now in Home-Directory

--- src/main/java/de/eventmanager/core/export/Exporter.java
+++ src/main/java/de/eventmanager/core/export/Exporter.java
@@ -5,17 +5,17 @@ import helper.LoggerHelper;
 import net.fortuna.ical4j.data.CalendarOutputter;
 import net.fortuna.ical4j.model.Calendar;
 import net.fortuna.ical4j.model.ValidationException;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Paths;
 
 public class Exporter {
 
     public static boolean exportEvent(Calendar calendar) {
         try {
-            FileOutputStream fout = new FileOutputStream("src/main/resources/mycalendar.ics");
+            String userHome = System.getProperty("user.home");
+            String downloadsPath = Paths.get(userHome, "Downloads", "mycalendar.ics").toString();
+            FileOutputStream fout = new FileOutputStream(downloadsPath);
             CalendarOutputter outputter = new CalendarOutputter();
             outputter.output(calendar, fout);
         } catch (IOException | ValidationException e) {
@@ -26,4 +26,5 @@ public class Exporter {
 
         return true;
     }
+
 }
