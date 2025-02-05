fddcb6bf3b50a533ddf406781d9b86c13ef144d8
Finn4239 <finn.oettinger@outlook.de>
2025-02-05 22:30:50 +0100

    Test: if Github-Actions failed with new check if method exportEvent() will be executed on local machine or on GitHub

--- src/main/java/de/eventmanager/core/export/Exporter.java
+++ src/main/java/de/eventmanager/core/export/Exporter.java
@@ -13,19 +13,26 @@ public class Exporter {
 
     public static boolean exportEvent(Calendar calendar) {
         try {
-            String userHome = System.getProperty("user.home");
-            String downloadsPath = Paths.get(System.getProperty("java.io.tmpdir"), "mycalendar.ics").toString();
-            //String downloadsPath = Paths.get(userHome, "Downloads", "mycalendar.ics").toString();
-            FileOutputStream fout = new FileOutputStream(downloadsPath);
+            String path;
+            if (System.getenv("CI") != null) {
+                // GitHub Actions: Safe in /tmp
+                path = Paths.get(System.getProperty("java.io.tmpdir"), "mycalendar.ics").toString();
+            } else {
+                // Locale Device: Safe in Downloads
+                String userHome = System.getProperty("user.home");
+                path = Paths.get(userHome, "Downloads", "mycalendar.ics").toString();
+            }
+
+            FileOutputStream fout = new FileOutputStream(path);
             CalendarOutputter outputter = new CalendarOutputter();
             outputter.output(calendar, fout);
         } catch (IOException | ValidationException e) {
             LoggerHelper.logErrorMessage(Exporter.class, e.getMessage());
-
             return false;
         }
 
         return true;
     }
+
 
 }
