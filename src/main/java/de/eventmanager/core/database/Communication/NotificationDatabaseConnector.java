f0e7e4e4aca8ba8701f921adf28754777ee4fcd3
Laura Dolibois <dolibois.laura@proton.me>
2025-02-06 20:58:17 +0100

    added class NotificationDatabaseConnector

---
+++ src/main/java/de/eventmanager/core/database/Communication/NotificationDatabaseConnector.java
@@ -0,0 +1,35 @@
+package de.eventmanager.core.database.Communication;
+
+import de.eventmanager.core.notifications.Notification;
+
+import java.util.Optional;
+
+public class NotificationDatabaseConnector {
+
+
+
+    //#region CRUD operations
+
+    public static boolean addNotification() {
+
+        return false;
+    }
+
+    public static Optional<Notification> readNotification() {
+
+        return Optional.empty();
+    }
+
+    public static boolean updateNotification() {
+
+        return false;
+    }
+
+    public static boolean deleteNotification() {
+
+        return false;
+    }
+
+    //#endregion CRUD operations
+
+}
