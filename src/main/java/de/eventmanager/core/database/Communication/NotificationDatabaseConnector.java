19d4630a5c528b3855932bd0127a0c4ca6e1e387
Laura Dolibois <dolibois.laura@proton.me>
2025-02-06 22:03:05 +0100

    added CRUD operations

--- src/main/java/de/eventmanager/core/database/Communication/NotificationDatabaseConnector.java
+++ src/main/java/de/eventmanager/core/database/Communication/NotificationDatabaseConnector.java
@@ -1,33 +1,159 @@
 package de.eventmanager.core.database.Communication;
 
 import de.eventmanager.core.notifications.Notification;
+import helper.LoggerHelper;
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Result;
+import org.jooq.impl.DSL;
 
+import java.sql.Connection;
+import java.util.ArrayList;
 import java.util.Optional;
+
+import static org.jooq.generated.tables.Notifications.NOTIFICATIONS;
+import static org.jooq.generated.tables.User.USER;
 
 public class NotificationDatabaseConnector {
 
+    //#region constants
 
+    private static final String NOTIFICATION_ADDED = "Notification added successfully";
+    private static final String NOTIFICATION_NOT_ADDED = "Error adding notification: ";
+    private static final String NOTIFICATION_NOT_READ = "Error reading notification: ";
+    private static final String NOTIFICATION_UPDATED = "Notification updated successfully";
+    private static final String NOTIFICATION_NOT_UPDATED = "Error updating notification: ";
+    private static final String NOTIFICATION_NOT_FOUND = "No notification found with the given ID";
+    private static final String NOTIFICATION_DELETED = "Notification deleted successfully";
+    private static final String NOTIFICATION_NOT_DELETED = "Error deleting notification: ";
+
+    //#endregion constants
 
     //#region CRUD operations
 
-    public static boolean addNotification() {
+    /**
+     * CREATE a notification
+     * */
+    public static boolean addNotification(Notification notification) {
+
+        try (Connection connection = DatabaseConnector.connect()) {
+
+            DSLContext create = DSL.using(connection);
+
+            int rowsAffected = create.insertInto(NOTIFICATIONS,
+                            NOTIFICATIONS.NOTIFICATIONID,
+                            NOTIFICATIONS.USERID,
+                            NOTIFICATIONS.MESSAGE,
+                            NOTIFICATIONS.MARKEDASREAD)
+                    .values(
+                            notification.getNotificationID(),
+                            notification.getUserID(),
+                            notification.getMessage(),
+                            notification.isMarkedAsRead())
+                    .execute();
+
+            if (rowsAffected > 0) {
+                LoggerHelper.logInfoMessage(UserDatabaseConnector.class, NOTIFICATION_ADDED);
+
+                return true;
+            }
+
+        } catch (Exception exception) {
+            LoggerHelper.logErrorMessage(UserDatabaseConnector.class, NOTIFICATION_NOT_ADDED + exception.getMessage());
+        }
 
         return false;
     }
 
-    public static Optional<Notification> readNotification() {
+    /**
+     * READ a list of notifications by userID
+     * */
+    public static ArrayList<Notification> readNotificationsByUserID(String userID) {
+        ArrayList<Notification> notifications = new ArrayList<>();
 
-        return Optional.empty();
+        try (Connection connection = DatabaseConnector.connect()) {
+
+            DSLContext create = DSL.using(connection);
+
+            Result<Record> records = create.select()
+                    .from(NOTIFICATIONS)
+                    .where(NOTIFICATIONS.USERID.eq(userID))
+                    .fetch();
+
+            for (var record : records) {
+                Notification notification = new Notification(
+                        record.get(NOTIFICATIONS.NOTIFICATIONID),
+                        record.get(NOTIFICATIONS.USERID),
+                        record.get(NOTIFICATIONS.MESSAGE),
+                        record.get(NOTIFICATIONS.MARKEDASREAD)
+                );
+
+                notifications.add(notification);
+            }
+
+        } catch (Exception exception) {
+            LoggerHelper.logErrorMessage(EventDatabaseConnector.class, NOTIFICATION_NOT_READ + exception.getMessage());
+        }
+
+        return notifications;
     }
 
-    public static boolean updateNotification() {
+    /**
+     * UPDATE a notification
+     * */
+    public static boolean updateNotification(Notification notification) {
 
-        return false;
+        try (Connection connection = DatabaseConnector.connect()) {
+
+            DSLContext create = DSL.using(connection);
+            int rowsUpdated = create.update(NOTIFICATIONS)
+                    .set(NOTIFICATIONS.MARKEDASREAD, notification.isMarkedAsRead())
+                    .where(NOTIFICATIONS.NOTIFICATIONID.eq(notification.getNotificationID()))
+                    .execute();
+
+            if (rowsUpdated > 0) {
+                LoggerHelper.logInfoMessage(UserDatabaseConnector.class, NOTIFICATION_UPDATED);
+
+                return true;
+            } else {
+                LoggerHelper.logInfoMessage(UserDatabaseConnector.class, NOTIFICATION_NOT_FOUND);
+
+                return false;
+            }
+
+        } catch (Exception exception) {
+            LoggerHelper.logErrorMessage(UserDatabaseConnector.class, NOTIFICATION_NOT_UPDATED + exception.getMessage());
+
+            return false;
+        }
     }
 
-    public static boolean deleteNotification() {
+    /**
+     * DELETE a notification by ID
+     * */
+    public static boolean deleteNotification(String notificationID) {
 
-        return false;
+        try (Connection connection = DatabaseConnector.connect()) {
+
+            DSLContext create = DSL.using(connection);
+
+            int rowsAffected = create.deleteFrom(NOTIFICATIONS)
+                    .where(NOTIFICATIONS.NOTIFICATIONID.eq(notificationID))
+                    .execute();
+
+            if (rowsAffected > 0) {
+                LoggerHelper.logInfoMessage(UserDatabaseConnector.class, NOTIFICATION_DELETED);
+            } else {
+                LoggerHelper.logInfoMessage(UserDatabaseConnector.class, NOTIFICATION_NOT_FOUND);
+            }
+
+            return rowsAffected > 0;
+
+        } catch (Exception exception) {
+            LoggerHelper.logErrorMessage(UserDatabaseConnector.class, NOTIFICATION_NOT_DELETED + exception.getMessage());
+
+            return false;
+        }
     }
 
     //#endregion CRUD operations
