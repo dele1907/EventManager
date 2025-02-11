5311b52e899889355c99251d4df7e20215607955
Timo Schmitz <timoxschmitz@gmail.com>
2025-02-11 17:51:07 +0100

    Change DateOperationsHelperTestDrive.java to be independence of the Database

--- src/test/java/helpertests/DateOperationsHelperTestDrive.java
+++ src/test/java/helpertests/DateOperationsHelperTestDrive.java
@@ -1,5 +1,7 @@
 package helpertests;
 
+import de.eventmanager.core.database.Communication.UserDatabaseConnector;
+import de.eventmanager.core.users.User;
 import helper.DateOperationsHelper;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
@@ -8,6 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class DateOperationsHelperTestDrive {
     //TODO: Rework test class for independence of the Database
+    User testUser;
 
     @Test
     void testCorrectEmail() {
@@ -20,13 +23,20 @@ public class DateOperationsHelperTestDrive {
     }
 
     @Test
-    void testAgeLogic() {
+    void testAgeLogicNew() {
+
+        testUser = new User("createTestUserDatabaseConnector", "Max", "Mustermann", "1999-01-10",
+                "max.create@testmail.com", "Password123", "1234567890", false);
+
+        UserDatabaseConnector.createNewUser(testUser);
 
         DateOperationsHelper dateOperationsHelper = new DateOperationsHelper();
 
-        int correcttestcase = dateOperationsHelper.getTheAgeFromDatabase("tisc00006@htwsaar.de");
+        int correcttestcase = dateOperationsHelper.getTheAgeFromDatabase("max.create@testmail.com");
 
         assertFalse(correcttestcase > 27);
         assertTrue(correcttestcase > 18);
+
+        UserDatabaseConnector.deleteUserByID("createTestUserDatabaseConnector");
     }
 }
