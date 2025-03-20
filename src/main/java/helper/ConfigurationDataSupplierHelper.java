d14d87b70a96d6bbee594d9316850f587b537d00
dennisleister <dennleist@gmail.com>
2025-03-20 22:11:09 +0100

    refactor: update production mode flag and adjust JDBC URL for test database

--- src/main/java/helper/ConfigurationDataSupplierHelper.java
+++ src/main/java/helper/ConfigurationDataSupplierHelper.java
@@ -3,7 +3,7 @@ package helper;
 public class ConfigurationDataSupplierHelper {
     public static final String REGISTER_NEW_USER_ID = "newRegisteredUser";
 
-    public static boolean IS_PRODUCTION_MODE = true;
+    public static boolean IS_PRODUCTION_MODE = false;
 
     public static void setIsProductionMode(boolean isProductionMode) {
         IS_PRODUCTION_MODE = isProductionMode;
