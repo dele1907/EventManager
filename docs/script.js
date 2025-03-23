52706671e89e423d5f740218dbfd678e6a617f1b
dele1907 <dennleist@gmail.com>
2025-03-23 17:34:58 +0100

    basic setup for website

---
+++ docs/script.js
@@ -0,0 +1,10 @@
+/**
+document.getElementById('program-video').addEventListener('click', function() {
+    var video = document.getElementById('program-video');
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+});*/
+
